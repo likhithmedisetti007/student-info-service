cd52ac80e470028fd94f41da0bb6a7fea5f462e6
Likhith Medisetti <likkyisalwayslucky@gmail.com>
2022-06-25 14:11:38 +0530

    Student Info Service created

---
+++ src/main/java/com/likhith/student/info/StudentInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.likhith.student.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/likhith/student/info/controller/StudentInfoController.java
@@ -0,0 +1,5 @@
+package com.likhith.student.info.controller;
+
+public class StudentInfoController {
+
+}
