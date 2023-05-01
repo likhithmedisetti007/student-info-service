43b28ba0e04906944857544d449b058d88288e9d
likky <likky@192.168.0.23>
2023-05-01 12:18:07 +0530

    GraphQL Added

--- src/main/java/com/likhith/student/info/entity/StudentInfoEntity.java
+++ src/main/java/com/likhith/student/info/entity/StudentInfoEntity.java
@@ -2,20 +2,21 @@ package com.likhith.student.info.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table(name = "STUDENT_INFO", schema = "LIKHITH")
+@Table(name = "STUDENT_INFO")
 @Data
 public class StudentInfoEntity {
 
 	@Id
-	@Column(name = "ID", nullable = false)
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "StudentInfoIdSequenceGenerator")
-//	@SequenceGenerator(name = "StudentInfoIdSequenceGenerator", sequenceName = "STUDENT_INFO_ID_SEQ")
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	String id;
 
 	@Column(name = "NAME")
--- src/main/java/com/likhith/student/info/repository/StudentInfoRepository.java
+++ src/main/java/com/likhith/student/info/repository/StudentInfoRepository.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.likhith.student.info.entity.StudentInfoEntity;
 
 @Repository
+@Transactional
 public interface StudentInfoRepository extends JpaRepository<StudentInfoEntity, String> {
 
 	public Optional<StudentInfoEntity> findById(String id);
